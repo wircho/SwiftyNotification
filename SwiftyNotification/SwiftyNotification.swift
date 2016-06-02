5ade6297a1e365328350896a2f55f61b4107da13
AdolfoX Rodriguez <adolfox.rodriguez@intel.com>
2016-06-01 23:43:04 -0400

    Added class and sample

---
+++ SwiftyNotification/SwiftyNotification.swift
@@ -0,0 +1,63 @@
+//---
+
+import Foundation
+
+// MARK: Notification Protocol
+
+public protocol Notification {
+    associatedtype P
+    static var name:String { get }
+    static func note(params:P) throws -> NSNotification
+    static func parameters(note:NSNotification) throws -> P
+}
+
+extension Notification {
+    public static func note(params:P) throws -> NSNotification {
+        throw NotificationError.MethodNotImplemented
+    }
+    public static func register<T:AnyObject>(listener:T, object:AnyObject? = nil, closure:(T,P)->Void) -> NotificationToken {
+        var token:NotificationToken? = nil
+        let observer = NSNotificationCenter.defaultCenter().addObserverForName(name, object: object, queue: nil) {
+            note in
+            guard let actualToken = token else { return }
+            guard let listener = actualToken.listener as? T else {
+                actualToken.cancel()
+                token = nil
+                return
+            }
+            guard let params = try? parameters(note) else { return }
+            closure(listener,params)
+        }
+        token = NotificationToken(listener: listener, observer: observer)
+        return token!
+    }
+    public static func post(params:P) throws -> Void {
+        let note = try self.note(params)
+        NSNotificationCenter.defaultCenter().postNotification(note)
+    }
+}
+
+// MARK: Notification Token
+
+public class NotificationToken {
+    private weak var listener:AnyObject?
+    private var observer:NSObjectProtocol?
+    private init(listener:AnyObject, observer:NSObjectProtocol) {
+        self.listener = listener
+        self.observer = observer
+    }
+    deinit {
+        self.cancel()
+    }
+    func cancel() {
+        guard let observer = observer else { return }
+        self.observer = nil
+        NSNotificationCenter.defaultCenter().removeObserver(observer)
+    }
+}
+
+// MARK: Notification Error
+
+public enum NotificationError: ErrorType {
+    case MethodNotImplemented
+}
---
+++ SwiftyNotificationSample/SwiftyNotificationSample/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  SwiftyNotificationSample
+//
+//  Created by AdolfoX Rodriguez on 2016-06-01.
+//  Copyright © 2016 Mokriya. All rights reserved.
+//
+
+import UIKit
+
+// MARK: Preparation
+
+// Keyboard Appeared Notification
+final class KeyboardAppeared: Notification {
+    static let name = UIKeyboardDidShowNotification
+    static func parameters(note: NSNotification) throws -> (beginFrame:CGRect,endFrame:CGRect) {
+        let beginFrame = (note.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() ?? CGRectNull
+        let endFrame = (note.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue() ?? CGRectNull
+        return (beginFrame,endFrame)
+    }
+}
+
+// Keyboard Disappeared Notification
+final class KeyboardDisappeared: Notification {
+    static let name = UIKeyboardDidHideNotification
+    static func parameters(note: NSNotification) throws -> (beginFrame:CGRect,endFrame:CGRect) {
+        let beginFrame = (note.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() ?? CGRectNull
+        let endFrame = (note.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue() ?? CGRectNull
+        return (beginFrame,endFrame)
+    }
+}
+
+class ViewController: UIViewController {
+
+    // MARK: IBOutlets & IBActions
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Registering as observer
+        // No need to unregister, since that will happen
+        // once the view controller gets released
+        
+        KeyboardAppeared.register(self) {
+            innerSelf,params in
+            
+            innerSelf.label.text = "Keyboard Appeared\nFrom \(params.beginFrame)\nTo \(params.endFrame)"
+        }
+        
+        KeyboardDisappeared.register(self) {
+            innerSelf,params in
+            
+            innerSelf.label.text = "Keyboard Disappeared\nFrom \(params.beginFrame)\nTo \(params.endFrame)"
+        }
+    }
+}
+
